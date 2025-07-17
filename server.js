7fed8ae3679cbec3d2bffa8bac57fd91c433f8a8
Kishorevijay07 <kishorevijay978@gmail.com>
2025-07-18 02:01:15 +0530

    URL Changed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://job-portal-frontend-pi-nine.vercel.app",
   credentials:true
 }))
 app.use(express.json(
